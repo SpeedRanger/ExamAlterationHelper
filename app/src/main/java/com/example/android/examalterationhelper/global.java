6162df639d66286e9622845007fd367677515c6f
SpeedRanger <kajaykumar20@gmail.com>
2020-03-15 19:50:41 +0530

    Added requests for alteration.

--- app/src/main/java/com/example/android/examalterationhelper/global.java
+++ app/src/main/java/com/example/android/examalterationhelper/global.java
@@ -1,26 +1,12 @@
 package com.example.android.examalterationhelper;
 
 public class global {
-    public static final String username, password;
-    public static final String date;
-    public static final String eroom, ename, etime, eblock;
-    public static final String req_date, req_time;
-    public static final String upreq_name, req_response;
-    public static final String[] upreq_res = new String[50];
-    public static final int pos;
+    public static String username, password;
+    public static String date;
+    public static String eroom, ename, etime, eblock;
+    public static String req_date, req_time;
+    public static String upreq_name, req_response;
+    public static String[] upreq_res = new String[50];
+    public static int pos;
 
-    static {
-        username = "";
-        password = "";
-        date = "";
-        eroom = "";
-        ename = "";
-        etime = "";
-        eblock = "";
-        req_date = "";
-        req_response = "";
-        req_time = "";
-        upreq_name = "";
-        pos = 0;
-    }
 }
